f0e39eb742abeb3e987f12815d46b00785fd1bd3
PNTDEV\VitorC <ues@pontual.pt>
2021-12-14 15:53:27 +0000

    Project init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+        }
+
+        private static void WriteSomething()
+        {
+            Console.WriteLine("Hello World!! :)");
+            Console.WriteLine("Press to continue !!");
+            Console.ReadKey();
+        }
+    }
+}
